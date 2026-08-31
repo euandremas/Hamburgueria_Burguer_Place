ec3dc97c6a8cfc5c124bc1fdd944ebe560467b66
Andre Santos <euandremas@gmail.com>
2026-08-31 20:30:42 -0300

    ci: configura pipeline de testes cobertura e build

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,9 +1,24 @@
 module.exports = {
     testEnvironment: "node",
+
     collectCoverageFrom: [
         "src/**/*.js",
         "!src/server.js"
     ],
+
     coverageDirectory: "coverage",
-    coverageReporters: ["text", "html"]
+
+    coverageReporters: [
+        "text",
+        "html"
+    ],
+
+    coverageThreshold: {
+        global: {
+            statements: 80,
+            functions: 80,
+            lines: 80,
+            branches: 60
+        }
+    }
 };
